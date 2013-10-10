3777f767276dc90f4b88e8ed6c3f40379f116716
Derek Neil <derekrneil@gmail.com>
2013-10-10 19:00:55 -0300

    blank highScoreController

---
+++ UnrealSimon/HighScoresController.h
@@ -0,0 +1,13 @@
+//
+//  HighScoresController.h
+//  UnrealSimon
+//
+//  Created by Derek Neil on 2013-10-10.
+//  Copyright (c) 2013 DKN Teck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HighScoresController : NSObject
+
+@end
---
+++ UnrealSimon/HighScoresController.m
@@ -0,0 +1,13 @@
+//
+//  HighScoresController.m
+//  UnrealSimon
+//
+//  Created by Derek Neil on 2013-10-10.
+//  Copyright (c) 2013 DKN Teck. All rights reserved.
+//
+
+#import "HighScoresController.h"
+
+@implementation HighScoresController
+
+@end
